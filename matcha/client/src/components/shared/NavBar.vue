e053d77fd269b5da3b295fc6d1a69dd727049aad
supalarry <lauris.skraucis@gmail.com>
2020-09-15 12:37:58 +0300

    latest section update

--- matcha/client/src/components/shared/NavBar.vue
+++ matcha/client/src/components/shared/NavBar.vue
@@ -13,7 +13,7 @@
       </div>
       <div id="burger" class="sm:hidden">
         <button type="button" class="block text-gray-800 focus:outline-none" v-on:click="isOpen = !isOpen">
-          <svg class="w-8 h-8 text-gray-matcha fill-current" viewBox="0 0 24 24">
+          <svg class="w-8 h-8 text-purple-matcha fill-current" viewBox="0 0 24 24">
             <path v-if="!isOpen" fill-rule="evenodd" d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"/>
             <path v-if="isOpen" fill-rule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"/>
           </svg>
@@ -21,9 +21,9 @@
       </div>
     </div>
     <div id="links" v-on:click="isOpen = !isOpen" v-bind:class="isOpen ? 'block' : 'hidden'" class="px-2 pb-5 text-center sm:p-0 sm:block sm:flex sm:items-center">
-      <div class="sm:flex sm:bg-gray-matcha sm:border-2 sm:border-gray-matcha sm:rounded-md">
-        <router-link v-if="!loggedIn" to="/login" class="navigation-button-logged-in sm:hover:bg-white-matcha sm:hover:text-gray-matcha sm:text-gray-matcha sm:bg-white-matcha sm:py-2 sm:px-8 sm:rounded-md">Sign In</router-link>
-        <router-link v-if="!loggedIn" to="/register" class="navigation-button-logged-in sm:hover:bg-gray-matcha sm:hover:text-white-matcha sm:text-white-matcha sm:py-2 sm:px-8">Get Started</router-link>
+      <div class="sm:flex sm:bg-purple-matcha sm:border-2 sm:border-purple-matcha sm:rounded-md">
+        <router-link v-if="!loggedIn" to="/login" class="navigation-button-logged-in sm:hover:bg-white-matcha sm:hover:text-purple-matcha sm:text-purple-matcha sm:bg-white-matcha sm:py-2 sm:px-8 sm:rounded-md">Sign In</router-link>
+        <router-link v-if="!loggedIn" to="/register" class="navigation-button-logged-in sm:hover:bg-purple-matcha sm:hover:text-white-matcha sm:text-white-matcha sm:py-2 sm:px-8">Get Started</router-link>
       </div>
       <router-link v-if="loggedIn" to="/search" class="navigation-button-logged-in">Search</router-link>
       <router-link v-if="loggedIn" to="/add" class="navigation-button-logged-in">Add</router-link>
--- matcha/client/src/views/Home.vue
+++ matcha/client/src/views/Home.vue
@@ -2,7 +2,7 @@
   <!-- eslint-disable max-len -->
   <!-- eslint-disable no-trailing-spaces -->
   <div class="home mx-4">
-  <section class="shadow-lg md:shadow-2xl mb-8 h-full flex rounded-lg">
+  <section class="shadow-lg mb-8 h-full flex rounded-lg">
     <div class="intro text-center bg-white-matcha rounded-lg w-full md:w-2/3
 flex flex-col justify-center md:rounded-tr-none md:rounded-br-none">
       <h1 class="text-5xl sm:text-6xl font-semibold text-gray-matcha">Someone<br>
@@ -10,7 +10,7 @@ flex flex-col justify-center md:rounded-tr-none md:rounded-br-none">
         for you</h1>
       <h1 class="text-2xl text-gray-matcha pb-10 pt-6">Matcha is a free dating<br>
         app to find you a lover</h1>
-      <button class="button-purple">Sign up</button>
+      <button class="button-purple">Get started</button>
     </div>
 
     <div class="image hidden md:inline-block overflow-hidden md:w-1/3">
@@ -18,21 +18,20 @@ flex flex-col justify-center md:rounded-tr-none md:rounded-br-none">
     </div>
   </section>
 
-  <section class="w-full md:flex mb-8">
-    <div class="md:inline-block md:w-2/3 mb-8 md:mb-0">
-      <div class="relative rounded-lg" style="padding-bottom: 100%">
-        <img src="../assets/home/couple2.jpg" class="absolute object-cover h-full w-full rounded-lg md:rounded-tr-none md:rounded-br-none">
+    <div class="pitch shadow-lg rounded-lg py-8 px-8 mb-8 w-full flex flex-col justify-center lg:px-16">
+      <h1 class="text-4xl md:text-6xl py-8 text-white-matcha text-center font-bold">Set up dates effortlessly</h1>
+      <h1 class="text-2xl py-4 text-white-matcha text-center">No more awkward approaches</h1>
+      <h1 class="text-2xl py-4 text-white-matcha text-center">No more lonely Friday nights</h1>
+    </div>
+
+    <div class="w-full mb-8 rounded-lg shadow-lg">
+      <div class="relative rounded-lg" style="padding-bottom: 70%">
+        <img src="../assets/home/couple2.jpg" class="absolute object-cover h-full w-full rounded-lg">
         <h1 class="text-gray-matcha text-2xl sm:text-5xl lg:text-6xl font-bold absolute bottom-0 w-full p-8"><span class="bg-white-matcha">"I met Lauris via Matcha, and he
           is cooler than Brad Pitt fo sure" - Mary</span></h1>
       </div>
     </div>
-    <div class="bg-purple-matcha md:bg-white-matcha shadow-lg md:shadow-2xl rounded-lg py-8 px-8 md:w-1/3 flex flex-col justify-center
-md:rounded-tl-none md:rounded-bl-none lg:px-16">
-      <h1 class="text-4xl md:text-2xl md:font-normal md:text-gray-matcha py-4 text-white-matcha text-center font-bold">Set up dates effortlessly</h1>
-      <h1 class="text-2xl py-4 text-white-matcha md:text-gray-matcha text-center">No awkward approaches</h1>
-      <h1 class="text-2xl py-4 text-white-matcha md:text-gray-matcha text-center">No more lonely evenings</h1>
-    </div>
-  </section>
+
   </div>
 </template>
 
@@ -48,6 +47,11 @@ export default {
 <style scoped>
 .home {
   height: calc(100% - 4rem - 2rem);
+}
+
+.pitch {
+  background-color: #6e72fc;
+  background-image: linear-gradient(315deg, #6e72fc 0%, #ad1deb 74%);
 }
 
 @responsive {
