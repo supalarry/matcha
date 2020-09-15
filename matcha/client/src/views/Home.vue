db19ec48e9072cae477d9db67a3f600fc4105689
supalarry <lauris.skraucis@gmail.com>
2020-09-15 11:05:09 +0300

    landing page new section

--- matcha/client/src/views/Home.vue
+++ matcha/client/src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <!-- eslint-disable max-len -->
   <!-- eslint-disable no-trailing-spaces -->
-  <section class="shadow-lg md:shadow-2xl mb-4 md:mb-0 home mx-4 flex rounded-lg">
-
+  <div class="home mx-4">
+  <section class="shadow-lg md:shadow-2xl mb-8 h-full flex rounded-lg">
     <div class="intro text-center bg-white-matcha rounded-lg w-full md:w-2/3
 flex flex-col justify-center md:rounded-tr-none md:rounded-br-none">
       <h1 class="text-5xl sm:text-6xl font-semibold text-gray-matcha">Someone<br>
@@ -17,6 +17,23 @@ flex flex-col justify-center md:rounded-tr-none md:rounded-br-none">
       <img src="../assets/home/couple.jpeg" class="w-full h-full object-cover rounded-tr-lg rounded-br-lg">
     </div>
   </section>
+
+  <section class="w-full md:flex mb-8">
+    <div class="md:inline-block md:w-2/3 mb-8 md:mb-0">
+      <div class="relative rounded-lg" style="padding-bottom: 100%">
+        <img src="../assets/home/couple2.jpg" class="absolute object-cover h-full w-full rounded-lg md:rounded-tr-none md:rounded-br-none">
+        <h1 class="text-gray-matcha text-2xl sm:text-5xl lg:text-6xl font-bold absolute bottom-0 w-full p-8"><span class="bg-white-matcha">"I met Lauris via Matcha, and he
+          is cooler than Brad Pitt fo sure" - Mary</span></h1>
+      </div>
+    </div>
+    <div class="bg-purple-matcha md:bg-white-matcha shadow-lg md:shadow-2xl rounded-lg py-8 px-8 md:w-1/3 flex flex-col justify-center
+md:rounded-tl-none md:rounded-bl-none lg:px-16">
+      <h1 class="text-4xl md:text-2xl md:font-normal md:text-gray-matcha py-4 text-white-matcha text-center font-bold">Set up dates effortlessly</h1>
+      <h1 class="text-2xl py-4 text-white-matcha md:text-gray-matcha text-center">No awkward approaches</h1>
+      <h1 class="text-2xl py-4 text-white-matcha md:text-gray-matcha text-center">No more lonely evenings</h1>
+    </div>
+  </section>
+  </div>
 </template>
 
 <script>
